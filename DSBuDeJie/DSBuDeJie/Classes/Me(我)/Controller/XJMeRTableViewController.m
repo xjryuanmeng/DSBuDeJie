1f45a6bee06b17e37d80b1e42b4598074ab49f85
XJR <1270508005@sina.com>
2016-07-20 22:22:34 +0800

    展示我的界面数据

--- DSBuDeJie/DSBuDeJie/Classes/Me(我)/Controller/XJMeRTableViewController.m
+++ DSBuDeJie/DSBuDeJie/Classes/Me(我)/Controller/XJMeRTableViewController.m
@@ -14,10 +14,14 @@
 #import "XJRSquareCell.h"
 
 static NSString * const ID = @"cell";
+static NSInteger const cols = 4;
+static CGFloat const margin = 1;
+#define itemWH (XJRScreenW - ((cols - 1) * margin)) / cols
 // 按钮选中状态 必须 手动设置
-@interface XJMeRTableViewController () <UICollectionViewDataSource>
 
-@property (nonatomic ,strong) NSArray *squareItems;
+@interface XJMeRTableViewController()<UICollectionViewDataSource>
+
+@property (nonatomic ,strong) NSMutableArray *squareItems;
 @property (nonatomic, weak) UICollectionView *collectionView;
 
 @end
@@ -37,24 +41,47 @@ static NSString * const ID = @"cell";
     //加载数据
     [self loadData];
 }
+// 处理数据
+- (void)resolveData
+{
+    // 判断缺几个
+    // 3 % 4 = 3 4 - 1 0
+    NSInteger count = self.squareItems.count;
+    NSInteger extre = count % cols;
+    if (extre) { // 补模型
+        extre = cols - extre;
+        for (int i = 0; i < extre; i++) {
+            XJRSquareItem *item = [[XJRSquareItem alloc] init];
+            [self.squareItems addObject:item];
+        }
+    }
+}
 -(void)loadData{
     //发送请求-->查看接口文档-->AFN
     AFHTTPSessionManager *mgr = [AFHTTPSessionManager manager];
     NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
     parameters[@"a"] = @"square";
     parameters[@"c"] = @"topic";
-    [mgr GET:@"http://api.budejie.com/api/api_open.php"  parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-        //NSLog(@"%@",responseObject);
-        [responseObject writeToFile:@"/Users/xingjingrong/Desktop/GIT/GitHub/DSBuDeJie/DSBuDeJie/DSBuDeJie/Classes/Me(我)/square.plist" atomically:YES];
+    [mgr GET:@"http://api.budejie.com/api/api_open.php"  parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, NSDictionary *  _Nullable responseObject) {
         // 字典数组
         NSArray *dictArr = responseObject[@"square_list"];
-        
         // 字典数组转换层模型数组
         _squareItems = [XJRSquareItem mj_objectArrayWithKeyValuesArray:dictArr];
-        
+        // 处理数据
+        [self resolveData];
         // 刷新表格
         [self.collectionView reloadData];
-        
+        // 4 5 9
+        // 设置collectionView高度 => 跟行数 => 总个数 行数:(count - 1) / cols + 1
+        NSInteger count = _squareItems.count;
+        NSInteger rows = (count - 1) / cols + 1;
+        CGFloat collectionH = rows * itemWH + (rows - 1) * margin;
+        self.collectionView.xjr_height = collectionH;
+        // 设置tableView滚动范围 => tableView滚动范围是系统会自动根据内容去计算
+        self.tableView.tableFooterView = self.collectionView;
+        //self.tableView.contentSize = CGSizeMake(0, CGRectGetMaxY(self.collectionView.frame));
+        [responseObject writeToFile:@"/Users/xingjingrong/Desktop/GIT/GitHub/DSBuDeJie/DSBuDeJie/DSBuDeJie/Classes/Me(我)/square.plist" atomically:YES];
+        //NSLog(@"%@",responseObject);
     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
         
     }];
@@ -67,24 +94,21 @@ static NSString * const ID = @"cell";
 -(void)setupFootView{
     //流水布局
     UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc]init];
-    CGFloat margin = 1;
-    NSInteger cols = 4;
-    //设置cell尺寸
-    CGFloat itemWH = (XJRScreenW - ((cols - 1) * margin)) / cols;
     layout.itemSize = CGSizeMake(itemWH, itemWH);
     //cell的间距
     layout.minimumInteritemSpacing = margin;
     layout.minimumLineSpacing = margin;
     //创建collectionView
-    UICollectionView *collectionView = [[UICollectionView alloc]initWithFrame:CGRectMake(0, 0, 0, 300) collectionViewLayout:layout];
-    collectionView.backgroundColor = XJRGlobleColor;
+    UICollectionView *collectionView = [[UICollectionView alloc]initWithFrame:CGRectMake(0, 0, 0, 845) collectionViewLayout:layout];
     _collectionView = collectionView;
+    collectionView.backgroundColor = XJRGlobleColor;
     self.tableView.tableFooterView = collectionView;
     // 设置数据源
     collectionView.dataSource = self;
     // 注册cell
     //[collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:ID];
     [collectionView registerNib:[UINib nibWithNibName:@"XJRSquareCell" bundle:nil] forCellWithReuseIdentifier:ID];
+    _collectionView.scrollEnabled = NO;
 }
 #pragma mark - UICollectionViewDataSource
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
